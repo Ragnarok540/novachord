197410d9c66e2112d84227df90d724c504cf9f58
Edgar Nova <ragnarok540@gmail.com>
2019-06-16 12:59:07 -0500

    basic chords

--- main.fs
+++ main.fs
@@ -3,33 +3,33 @@ open System
 let noteToNumber note =
     let number =
         match note with
-        | "B#" -> 0  | "C"  -> 0
-        | "C#" -> 1  | "Db" -> 1
-        | "D"  -> 2
-        | "D#" -> 3  | "Eb" -> 3
-        | "E"  -> 4  | "Fb" -> 4
-        | "E#" -> 5  | "F"  -> 5
-        | "F#" -> 6  | "Gb" -> 6
-        | "G"  -> 7
-        | "G#" -> 8  | "Ab" -> 8
-        | "A"  -> 9
-        | "A#" -> 10 | "Bb" -> 10
-        | "B"  -> 11 | "Cb" -> 11
+        | "B#" | "C"  -> 0
+        | "C#" | "Db" -> 1
+        | "D"         -> 2
+        | "D#" | "Eb" -> 3
+        | "E"  | "Fb" -> 4
+        | "E#" | "F"  -> 5
+        | "F#" | "Gb" -> 6
+        | "G"         -> 7
+        | "G#" | "Ab" -> 8
+        | "A"         -> 9
+        | "A#" | "Bb" -> 10
+        | "B"  | "Cb" -> 11
     number
 
 let numberToFlatNote number =
     let note =
         match number with
-        | 0 -> "C"
-        | 1 -> "Db"
-        | 2 -> "D"
-        | 3 -> "Eb"
-        | 4 -> "E"
-        | 5 -> "F"
-        | 6 -> "Gb"
-        | 7 -> "G"
-        | 8 -> "Ab"
-        | 9 -> "A"
+        | 0 ->  "C"
+        | 1 ->  "Db"
+        | 2 ->  "D"
+        | 3 ->  "Eb"
+        | 4 ->  "E"
+        | 5 ->  "F"
+        | 6 ->  "Gb"
+        | 7 ->  "G"
+        | 8 ->  "Ab"
+        | 9 ->  "A"
         | 10 -> "Bb"
         | 11 -> "B"
     note
@@ -37,57 +37,73 @@ let numberToFlatNote number =
 let numberToSharpNote number =
     let note =
         match number with
-        | 0 -> "C"
-        | 1 -> "C#"
-        | 2 -> "D"
-        | 3 -> "D#"
-        | 4 -> "E"
-        | 5 -> "F"
-        | 6 -> "F#"
-        | 7 -> "G"
-        | 8 -> "G#"
-        | 9 -> "A"
+        | 0 ->  "C"
+        | 1 ->  "C#"
+        | 2 ->  "D"
+        | 3 ->  "D#"
+        | 4 ->  "E"
+        | 5 ->  "F"
+        | 6 ->  "F#"
+        | 7 ->  "G"
+        | 8 ->  "G#"
+        | 9 ->  "A"
         | 10 -> "A#"
         | 11 -> "B"
     note
 
-let convertToFlat array = 
-    array
-    |> Array.map numberToFlatNote
+let nameToIntegerArray name =
+    let integerArray =
+        match name with
+        | "maj"     -> [|0; 4; 7|]
+        | "min"     -> [|0; 3; 7|]
+        | "aug"     -> [|0; 4; 8|]
+        | "dim"     -> [|0; 3; 6|]
+        | "maj6"    -> [|0; 4; 7; 9|]
+        | "min6"    -> [|0; 3; 7; 9|]
+        | "7"       -> [|0; 4; 7; 10|]
+        | "maj7"    -> [|0; 4; 7; 11|]
+        | "min7"    -> [|0; 3; 7; 10|]
+        | "aug7"    -> [|0; 4; 8; 10|]
+        | "dim7"    -> [|0; 3; 6; 9|]
+        | "m7b5"    -> [|0; 3; 6; 10|]
+        | "minmaj7" -> [|0; 3; 7; 11|]
+    integerArray
 
-let convertToSharp array = 
-    array
-    |> Array.map numberToSharpNote
+let convert (note:string) name =
+    let flat = note.Contains('b')
+    let sharp = note.Contains('#')
+    let func =
+        match name with
+        | "maj"  | "aug"  | "maj6"
+        | "maj7" | "aug7" ->
+            if flat then
+               numberToFlatNote
+            else
+               numberToSharpNote
+        | "min"  | "dim"  | "min6"
+        | "7"    | "min7" | "dim7"
+        | "m7b5" | "minmaj7"->
+            if sharp then
+                numberToSharpNote
+            else
+                numberToFlatNote
+    func
 
-let normalize number =
-    number % 12
-
-let major note = 
+let createChord note name =
     let noteNumber = noteToNumber note
+    let integerArray = nameToIntegerArray name
+    let notes = Array.create integerArray.Length noteNumber
+    let notesZip = Array.zip notes integerArray
+    let convertFunc = convert note name
     let result =
-        [|noteNumber; noteNumber + 4; noteNumber + 7|]
-        |> Array.map normalize
-    result
-
-let minor note = 
-    let noteNumber = noteToNumber note
-    let result =
-        [|noteNumber; noteNumber + 3; noteNumber + 7|]
-        |> Array.map normalize
+        notesZip
+        |> Array.map (fun (x, y) -> x + y)
+        |> Array.map (fun x -> x % 12)
+        |> Array.map convertFunc
     result
 
 [<EntryPoint>]
 let main(args) =
-    if args.[0] = "major" then
-        let majorChord = major args.[1]
-        if args.[1].Contains('b') then
-            printfn "Major Chord: %A" (convertToFlat majorChord)
-        else
-            printfn "Major Chord: %A" (convertToSharp majorChord)
-    elif args.[0] = "minor" then
-        let minorChord = minor args.[1]
-        if args.[1].Contains('#') then
-            printfn "Minor Chord: %A" (convertToSharp minorChord)
-        else
-            printfn "Minor Chord: %A" (convertToFlat minorChord)
+    let result = createChord args.[0] args.[1]
+    printfn "Chord: %A" result
     0
